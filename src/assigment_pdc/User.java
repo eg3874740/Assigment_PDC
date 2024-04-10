054c4d747ec2a37d08d6c75c04997addae6c88b1
ella <gregoryella1@gmail.com>
2024-04-11 11:08:37 +1200

    add a user class for to save the score of the user while playing the game

---
+++ src/assigment_pdc/User.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assigment_pdc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ellagregory
+ */
+public class User {
+    private String name;
+    private int score;
+    private static final String USERS_FILE = "resources/users.txt";
+
+    public User(String name) {
+        this.name = name;
+        this.score = 0;
+        loadScore();
+    }
+
+    private void loadScore() {
+        // Load the user's score from the file
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(USERS_FILE));
+            for (String line : lines) {
+                String[] parts = line.split(",");
+                if (parts[0].equals(name)) {
+                    score = Integer.parseInt(parts[1]);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while loading scores: " + e.getMessage());
+            // If the file does not exist, we'll start with a score of 0.
+        }
+    }
+
+    public void addScore(int points) {
+        score += points;
+        saveScore();
+    }
+
+    private void saveScore() {
+        // Save the user's score to the file
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(USERS_FILE));
+            List<String> updatedLines = new ArrayList<>();
+            boolean userFound = false;
+            for (String line : lines) {
+                String[] parts = line.split(",");
+                if (parts[0].equals(name)) {
+                    updatedLines.add(name + "," + score);
+                    userFound = true;
+                } else {
+                    updatedLines.add(line);
+                }
+            }
+            if (!userFound) {
+                updatedLines.add(name + "," + score);
+            }
+            Files.write(Paths.get(USERS_FILE), updatedLines);
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving scores: " + e.getMessage());
+        }
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+    
+    
+}
