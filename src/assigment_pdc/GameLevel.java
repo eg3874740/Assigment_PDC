7b5541a7b7d02f7ca1db6102b8f479e92331f698
doCode101 <164111266+doCode101@users.noreply.github.com>
2024-04-11 14:52:06 +1200

    Update GameLevel.java
    Add comments for readability

--- src/assigment_pdc/GameLevel.java
+++ src/assigment_pdc/GameLevel.java
@@ -13,17 +13,19 @@ import java.util.Random;
 public class GameLevel {
     private int level;
     private List<String> words;
-    
-     public GameLevel(int level) throws IOException {
+
+    public GameLevel(int level) throws IOException {
         this.level = level;
         loadWords();
     }
-     
+
+    //load words from word file, eg."level1", depending on the level you choose
     private void loadWords() throws IOException {
         String fileName = "resources/level" + level + ".txt";
         words = Files.readAllLines(Paths.get(fileName));
     }
-    
+
+    ////randomly select words
     public String selectWord() {
         return words.get(new Random().nextInt(words.size()));
     }
