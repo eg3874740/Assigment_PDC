f7a75edb5c742fbf71e088a5956786ecf531e47f
doCode101 <164111266+doCode101@users.noreply.github.com>
2024-04-12 11:56:34 +1200

    Update Words.java
    Relevant packages for Words.java class

--- src/assigment_pdc/Words.java
+++ src/assigment_pdc/Words.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class Words {
     
     private Map<Integer, List<String>> levelWords;
