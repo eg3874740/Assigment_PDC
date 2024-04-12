b71532aedf9a9e3543a026a753346cb00d81614a
doCode101 <164111266+doCode101@users.noreply.github.com>
2024-04-12 12:00:43 +1200

    Update GameLevel.java
    Updating class to account for Words.java reading the words from file, making GameLevel.java responsible for assigning appropriate words to certain levels at random according to user's level choice.

--- src/assigment_pdc/GameLevel.java
+++ src/assigment_pdc/GameLevel.java
@@ -1,34 +1,24 @@
-package Assignment_1;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Random;
 
-/**
- *
- * @author doris
- */
 public class GameLevel {
     private int level;
     private List<String> words;
 
-    public GameLevel(int level) throws IOException {
+    public GameLevel(int level, Words wordBank) {
         this.level = level;
-        loadWords();
+        this.words = wordBank.getWordsForLevel(level);
     }
 
-    //load words from word file, eg."level1", depending on the level you choose
-    private void loadWords() throws IOException {
-        String fileName = "resources/level" + level + ".txt";
-        words = Files.readAllLines(Paths.get(fileName));
-    }
-
-    ////randomly select words
     public String selectWord() {
+        if (words.isEmpty()) {
+            System.out.println("No words loaded for level " + level);
+            return ""; // Return an empty string if no words are loaded
+        }
         return words.get(new Random().nextInt(words.size()));
     }
-    
-    
 }
+
+
+
+
