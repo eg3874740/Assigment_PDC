318e8a8b7ce31e45c59aa54f408890e1bab87151
doCode101 <164111266+doCode101@users.noreply.github.com>
2024-06-06 11:21:19 +1200

    Update Scoreboard.java
    Adding comments to the methods of the Scoreboard.java class

--- src/assigment_pdc/Scoreboard.java
+++ src/assigment_pdc/Scoreboard.java
@@ -1,11 +1,16 @@
+//The Scoreboard class is responsible for maintaining and displaying the scores of users.
+
 public class Scoreboard {
   
+  // A list of users whose scores are being tracked
     private final List<User> users;
 
+  //Constructs a Scoreboard with the specified list of users.
     public Scoreboard(List<User> users) {
         this.users = users;
     }
 
+  //Displays the scores of all users in the scoreboard.
     public void displayScores() {
         System.out.println("===== Scoreboard =====");
         for (User user : users) {
