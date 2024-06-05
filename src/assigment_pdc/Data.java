90a398e8bfa55a3252522f486a6e867efd060728
eg3874740 <131919905+eg3874740@users.noreply.github.com>
2024-06-05 12:45:41 +1200

    Create Data.java
    This class called Data, will contain the application's data fields, such as user login status, quiz information, and game state.

---
+++ src/assigment_pdc/Data.java
@@ -0,0 +1,11 @@
+//This class called Data, will contain the application's data fields, such as user login status, quiz information, and game state. 
+
+
+public class Data {
+    boolean loginFlag = false;
+    boolean quitFlag = false;
+    int currentScore = 0;
+    String question;
+    String[] choices;
+    String correctAnswer;
+}
