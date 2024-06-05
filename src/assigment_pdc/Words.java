87c43fb73b6fb8dd3e402e3ea9e72ae6c6306b4c
eg3874740 <131919905+eg3874740@users.noreply.github.com>
2024-06-05 21:36:08 +1200

    Update Words.java
    doesn't need any changes yet

--- src/assigment_pdc/Words.java
+++ src/assigment_pdc/Words.java
@@ -4,6 +4,7 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 public class Words {
     
     private Map<Integer, List<String>> levelWords;
