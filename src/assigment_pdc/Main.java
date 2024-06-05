06a5a6aee3efcffaae1898cee2b38b717df91ba7
eg3874740 <131919905+eg3874740@users.noreply.github.com>
2024-06-05 21:31:04 +1200

    Update Main.java (Entry point of the GUI)

--- src/assigment_pdc/Main.java
+++ src/assigment_pdc/Main.java
@@ -1,57 +1,11 @@
-// this is our main method to ask the user for inputs to reviece 
-import java.util.Scanner;
-
+// ENTRY POINT OF MY GUI
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        GameManager gameManager = new GameManager();
-
-        //Introduction message
-        System.out.println("Welcome to our Hangman game (without the hangman)!");
-
-        // Ask the user for their name
-        System.out.print("Please enter your name: ");
-        String username = scanner.nextLine();
-
-        boolean play = true;
-        while (play) {
-            GameMenu.displayMenu();
-            System.out.println("Choose an option: " + username + "!");
-            String option = scanner.nextLine();
-            switch (option) {
-                case "1":
-                    System.out.println("Please choose your level 1, 2, or 3: ");
-                    int level = Integer.parseInt(scanner.nextLine());
-                    if (level < 1 || level > 3) {
-                        System.out.println("Invalid level. Please choose 1, 2, or 3.");
-                        break;
-                    }
-                    User user = new User(username); // Use the user's name
-                    gameManager.addUser(user);
-                    gameManager.startGame(user, level);
-                    break;
-                case "2":
-                    System.out.println("Scores:");
-                    gameManager.displayScores();
-                    break;
-                case "3":
-                    System.out.println("Viewing Game Statistics:");
-                    gameManager.displayGameStats(); // Call to display game statistics
-                    break;
-                case "4":
-                    play = false;
-                    break;
-                default:
-                    System.out.println("Invalid option. Please choose again.");
-                    break;
+        
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new HangmanGUI();
             }
-        }
-
-        System.out.println("Thank you for playing!");
-        scanner.close();
+        });
     }
 }
-//Second part of the assigment now we have to implement our project into a GUI interFace 
-// lets use the code from labs as a platform for our new code 
-// i am goimg to create a database first for our code called ella&doris 
-// database successful
