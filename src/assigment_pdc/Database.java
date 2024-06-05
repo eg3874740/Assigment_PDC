9e82091502c73567f9958463afba66760a0f2da9
eg3874740 <131919905+eg3874740@users.noreply.github.com>
2024-06-05 12:22:02 +1200

    Created Database

--- src/assigment_pdc/Database.java
+++ src/assigment_pdc/Database.java
@@ -1,1 +1,17 @@
 // this going to be our database file 
+// first lets connect the database
+public class Database {
+    
+    Connection conn = null;
+    String url = "jdbc:derby://localhost:1527/PDC_Project 2";
+    String dbusername = "ella";
+    String dbpassword = "doris";
+    
+    
+    public Database() {
+        try {
+            conn = DriverManager.getConnection(url, dbusername, dbpassword);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
